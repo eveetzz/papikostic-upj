9053617fb78d81a281de775c384e9f28e937c641
eveetzz <eveetzz>
2026-06-05 15:12:26 +0700

    benerin footer

--- src/pages/admin/AdminLayout.jsx
+++ src/pages/admin/AdminLayout.jsx
@@ -6,12 +6,15 @@ import { Footer } from "../../components/Footer";
 
 export const Admin = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <div className="flex h-screen overflow-hidden">
+      <div className="flex h-screen overflow-hidden bg-gray-50">
         <NavbarAdmin />
-        <main className="flex-1 overflow-y-auto">
-          <Outlet />
+        <main className="flex-1 overflow-y-auto flex flex-col justify-between">
+          <div className="flex-grow">
+            <Outlet />
+          </div>
           <Footer />
         </main>
       </div>
